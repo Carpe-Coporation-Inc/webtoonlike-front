bb92800814a8b395ff9678ea4474c24db5ee3856
d <170926579+dddddl12@users.noreply.github.com>
2024-12-04 10:41:37 +0900

    Add Neon database adapter for Prisma
    Integrated the @prisma/adapter-neon with Prisma in the project, allowing usage of Neon database as an optional adapter when the database URL includes ".neon.tech". Adjusted the Prisma client configuration to support connection pooling using '@neondatabase/serverless' and the 'ws' library for WebSocket connections. Updated 'package.json' and 'pnpm-lock.yaml' to include the necessary dependencies for these changes.

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,22 +1,29 @@
-// ts-ignore 7017 is used to ignore the error that the global object is not
-// defined in the global scope. This is because the global object is only
-// defined in the global scope in Node.js and not in the browser.
-
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+neonConfig.webSocketConstructor = ws;
 
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  // log: ["query", "info", "warn", "error"],
+// todo 로깅 이슈
+// todo pool 개수 체크
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_PRISMA_URL
 });
+const adapter = process.env.POSTGRES_PRISMA_URL?.includes(".neon.tech")
+  // neon db를 사용할 때만 해당 어댑터 사용
+  ? new PrismaNeon(pool) : null;
 
-// if (process.env.NODE_ENV !== "production")
-globalForPrisma.prisma = prisma;
+// 로컬 개발 시 singleton 사용
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma
+  = globalForPrisma.prisma
+  || new PrismaClient({
+    adapter,
+    // Optional logging for debugging:
+    // log: ["query", "info", "warn", "error"],
+  });
+
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = prisma;
 
 export default prisma;
